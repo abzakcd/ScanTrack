cf450b731a5250c53e0145ff827ec7ba3021a65c
Yanal Abzak <131192823+abzakcd@users.noreply.github.com>
2024-08-17 02:23:39 +0300

    add react full

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import './styles/main.css';
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            <HomePage />
+        </div>
+    );
+}
+
+export default App;
