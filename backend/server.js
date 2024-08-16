aa9d26aa75e9bf1026faf3727c067ec09d75d19c
Yanal Abzak <131192823+abzakcd@users.noreply.github.com>
2024-08-17 01:54:20 +0300

    all base for user

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
-const app = require('./app');
-const port = process.env.PORT || 3000;
+const express = require('express');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
 
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Routes
+app.use('/auth', authRoutes);
+
+// Start the server
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
